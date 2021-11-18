6a6d9522e60a4e9922470c8300c3d82df7e15971
Dylan Cao <dylan.v.cao@gmail.com>
2021-11-18 14:55:36 -0500

    Update to save/load models given model dir

--- training_efficientnet_b1.py
+++ training_efficientnet_b1.py
@@ -1,12 +1,27 @@
 import tensorflow as tf
-model = tf.keras.applications.efficientnet.EfficientNetB1(weights=None,classes=2)
-train_ds = tf.keras.preprocessing.image_dataset_from_directory('Classification_data_augmented/Training', labels='inferred', image_size=(240,240), label_mode='categorical', batch_size=16)
-test_ds = tf.keras.preprocessing.image_dataset_from_directory('Classification_data_augmented/Testing1', labels='inferred', image_size=(240,240), label_mode='categorical', batch_size=16)
-from tensorflow.keras import optimizers
-model.compile(
-    loss="categorical_crossentropy",
-    optimizer='adam',
-    metrics=["acc"],
-)
-model.fit(train_ds, epochs=10)
-print(model.evaluate(test_ds))
+import argparse
+from tensorflow import keras
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode')
+    parser.add_argument('model_path')
+    parser.add_argument('ds_path')
+    args = parser.parse_args()
+    if args.mode == 'train':
+        model = tf.keras.applications.efficientnet.EfficientNetB1(weights=None,classes=2)
+        train_ds = tf.keras.preprocessing.image_dataset_from_directory(f'%s/Training' % args.ds_path, labels='inferred', image_size=(240,240), label_mode='categorical', batch_size=16)
+        model.compile(
+            loss="categorical_crossentropy",
+            optimizer='adam',
+            metrics=["acc"],
+        )
+        model.fit(train_ds, epochs=10)
+        model.save(args.model_path)
+    elif args.mode == 'test':
+        test_ds = tf.keras.preprocessing.image_dataset_from_directory(f'%s/Testing1' % args.ds_path, labels='inferred', image_size=(240,240), label_mode='categorical', batch_size=16)
+        model = keras.models.load_model(args.model_path)
+        print(model.evaluate(test_ds))
+
+if __name__ == '__main__':
+    main()
--- training_mobilenet_v2.py
+++ training_mobilenet_v2.py
@@ -1,13 +1,27 @@
 import tensorflow as tf
+import argparse
+from tensorflow import keras
 
-model = tf.keras.applications.MobileNetV2(weights=None, input_shape=(256,256,3), classes=2)
-train_ds = tf.keras.preprocessing.image_dataset_from_directory('Classification_data_augmented/Training', labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
-test_ds = tf.keras.preprocessing.image_dataset_from_directory('Classification_data_augmented/Testing1', labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
-from tensorflow.keras import optimizers
-model.compile(
-    loss="categorical_crossentropy",
-    optimizer='adam',
-    metrics=["acc"],
-)
-model.fit(train_ds, epochs=10)
-print(model.evaluate(test_ds))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode')
+    parser.add_argument('model_path')
+    parser.add_argument('ds_path')
+    args = parser.parse_args()
+    if args.mode == 'train':
+        model = tf.keras.applications.MobileNetV2(weights=None, input_shape=(256,256,3), classes=2)
+        train_ds = tf.keras.preprocessing.image_dataset_from_directory(f'%s/Training' % args.ds_path, labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
+        model.compile(
+            loss="categorical_crossentropy",
+            optimizer='adam',
+            metrics=["acc"],
+        )
+        model.fit(train_ds, epochs=10)
+        model.save(args.model_path)
+    elif args.mode == 'test':
+        test_ds = tf.keras.preprocessing.image_dataset_from_directory(f'%s/Testing1' % args.ds_path, labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
+        model = keras.models.load_model(args.model_path)
+        print(model.evaluate(test_ds))
+
+if __name__ == '__main__':
+    main()
