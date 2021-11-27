803345e86c843f591a9ec278de2a931277c1b317
Dylan Cao <dylan.v.cao@gmail.com>
2021-11-26 22:29:13 -0500

    Update training_mobilenet_v2.py: Set BatchNorm layer momentum to 0.9 so it actually works, autogenerate checkpoints (save best F1 score)

--- training_mobilenet_v2.py
+++ training_mobilenet_v2.py
@@ -1,6 +1,9 @@
-import tensorflow as tf
 import argparse
-from tensorflow import keras
+
+def process(image,label):
+    import tensorflow as tf
+    image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
+    return image,label
 
 def main():
     parser = argparse.ArgumentParser()
@@ -8,20 +11,44 @@ def main():
     parser.add_argument('model_path')
     parser.add_argument('ds_path')
     args = parser.parse_args()
+    from tensorflow import keras
+    import tensorflow_addons as tfa
     if args.mode == 'train':
-        model = tf.keras.applications.MobileNetV2(weights=None, input_shape=(256,256,3), classes=2)
-        train_ds = tf.keras.preprocessing.image_dataset_from_directory(f'%s/Training' % args.ds_path, labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
+        train_ds = keras.preprocessing.image_dataset_from_directory(f'%s/Training' % args.ds_path, labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
+        train_ds = train_ds.map(process)
+        test_ds = keras.preprocessing.image_dataset_from_directory(f'%s/Testing1' % args.ds_path, labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
+        test_ds = test_ds.map(process)
+        model = keras.applications.MobileNetV2(input_shape=(256,256,3), classes=2, weights=None, classifier_activation='sigmoid')
+        for layer in model.layers:
+            if isinstance(layer, keras.layers.BatchNormalization):
+                print("Setting batch normalization layer")
+                layer.momentum = 0.9
+        model_checkpoint_callback = keras.callbacks.ModelCheckpoint(filepath=args.model_path, save_weights_only=True, monitor='val_f1_score', save_best_only=True, mode='max', verbose=1)
         model.compile(
-            loss="categorical_crossentropy",
-            optimizer='adam',
-            metrics=["acc"],
+            loss=keras.losses.CategoricalCrossentropy(),
+            optimizer=keras.optimizers.Adam(learning_rate=0.00003),
+            metrics=[keras.metrics.BinaryAccuracy(), keras.metrics.AUC(), tfa.metrics.F1Score(2, average='micro')],
         )
-        model.fit(train_ds, epochs=10)
-        model.save(args.model_path)
-    elif args.mode == 'test':
-        test_ds = tf.keras.preprocessing.image_dataset_from_directory(f'%s/Testing1' % args.ds_path, labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
-        model = keras.models.load_model(args.model_path)
+        model.fit(train_ds, epochs=100, validation_data=test_ds, callbacks=[model_checkpoint_callback])
+        print(model.predict(test_ds))
         print(model.evaluate(test_ds))
+    elif args.mode == 'test':
+        test_ds = keras.preprocessing.image_dataset_from_directory(f'%s/Testing1' % args.ds_path, labels='inferred', image_size=(256,256), label_mode='categorical', batch_size=32)
+        test_ds = test_ds.map(process)
+        model = keras.applications.MobileNetV2(input_shape=(256,256,3), classes=2, weights=None, classifier_activation='sigmoid')
+        for layer in model.layers:
+            if isinstance(layer, keras.layers.BatchNormalization):
+                print("Setting batch normalization layer")
+                layer.momentum = 0.9
+        model.load_weights(args.model_path)
+        model.compile(
+            loss=keras.losses.CategoricalCrossentropy(),
+            optimizer=keras.optimizers.Adam(learning_rate=0.00003),
+            metrics=[keras.metrics.BinaryAccuracy(), keras.metrics.AUC(), tfa.metrics.F1Score(2, average='micro')],
+        )
+        print(model.predict(test_ds))
+        print(model.evaluate(test_ds))
+
 
 if __name__ == '__main__':
     main()
